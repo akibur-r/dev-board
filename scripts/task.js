7826e7862804b6c30d7b2ea84aca436f198cc654
akibur-r <akibur.pciu@gmail.com>
2025-03-02 13:35:40 +0600

    Added alerts when task completed

--- scripts/task.js
+++ scripts/task.js
@@ -1,8 +1,16 @@
 const btnList = document.getElementsByClassName('completed-btn');
+const statusList = [];
+
 
 for(const btn of btnList) {
+    statusList.push(0);
     btn.addEventListener('click', function(event) {
         const btnItem = event.target.setAttribute('disabled', 'true');
         handleCompleted(btn.parentNode.parentNode.querySelector('h1'));
+        statusList.pop();
+        
+        if(statusList.length < 1) {
+            alert("Congrats!! You have completed all the current tasks");
+        }
     })
 }
